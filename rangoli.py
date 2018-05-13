b1fd21023d2c6b190847ba21e66850d185756316
ashdvlps <ashdvlps@gmail.com>
2018-05-13 03:21:28 -0700

    final version

--- rangoli.py
+++ rangoli.py
@@ -1,7 +1,22 @@
 from string import ascii_lowercase
-def rangs(n):
-	l = ascii_lowercase
-	s = l[0:n]
-	
 
-rangs(int(input()))
+def print_rangoli(size):
+    n = size
+    padding_length = 4*n - 3
+    s = ascii_lowercase[0:n][::-1]
+    a = 1
+    while a < len(s):
+        r = s[0:a][::-1]
+        rd = r[::-1]+r[1:]
+        print('-'.join(rd).center(padding_length, '-'))
+        a = a + 1
+
+    while a > 0:
+        p = s[0:a][::-1]
+        pd = p[::-1]+p[1:]
+        print('-'.join(pd).center(padding_length, '-'))
+        a = a - 1
+		
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
